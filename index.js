ade4fdb9724b46e9e7c694bd1bd9501b8aa3018b
zolev1984 <misael1984@gmail.com>
2023-06-28 00:57:34 -0400

    Configuring DB Planet Scale

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express')
+require('dotenv').config()
 
 const app = express()
+const mysql = require('mysql2')
 
 app.get('/', (req,res) => {
+    const connection = mysql.createConnection(process.env.DATABASE_URL)
+    console.log('Connected to PlanetScale!')
+    connection.end()
+    
     res.send('Hello world')
 })
+
+
+
+
+
+
 
 app.listen(process.env.PORT || 3000)
 console.log('Server listen in: ', process.env.PORT || 3000)
