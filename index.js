0f41035551f86c5d880c0c9f744e785801b7816d
zolev1984 <misael1984@gmail.com>
2023-06-28 00:25:45 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req,res) => {
+    res.send('Hello world')
+})
+
+app.listen(process.env.PORT || 3000)
+console.log('Server listen in: ', process.env.PORT || 3000)
